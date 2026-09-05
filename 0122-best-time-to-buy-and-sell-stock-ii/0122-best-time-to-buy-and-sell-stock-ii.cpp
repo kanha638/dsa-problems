64300cbc2bb6da6c7c14022e51c1ddb5da3b5115
KANHA TIWARI <74721653+kanha638@users.noreply.github.com>
2026-09-05 11:31:18 +0530

    Time: 0 ms (100%), Space: 20.1 MB (64.77%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
@@ -21,15 +21,21 @@ public:
     // }
     int maxProfit(vector<int>& prices) {
         int n = prices.size();
-        vector<vector<int>> dp(n+1,vector<int>(2,-1));
-        dp[n][0] = 0;
-        dp[n][1] = 0;
+        // vector<vector<int>> dp(n+1,vector<int>(2,-1));
+        // dp[n][0] = 0;
+        // dp[n][1] = 0;
+        
+        int prevToSell = 0;//space optmization
+        int prevToBuy = 0;
 
         for(int i = n-1;i>=0;i--){
-            dp[i][1] = max(-prices[i]+dp[i+1][0],dp[i+1][1]);
-            dp[i][0] = max(prices[i] + dp[i+1][1],dp[i+1][0]);
+            int currToBuy = max(-prices[i]+prevToSell,prevToBuy);
+            int currToSell = max(prices[i] + prevToBuy,prevToSell);
+            prevToBuy = currToBuy;
+            prevToSell = currToSell;
         }
+        return prevToBuy;
 
-        return dp[0][1];
+        // return dp[0][1];
     }
 };
